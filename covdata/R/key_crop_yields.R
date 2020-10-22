ba4b98d460c2935202f76f8ca38b698a7019d282
Linling bai <69233935+AntiRosa@users.noreply.github.com>
2020-10-23 07:09:39 +1100

    some word

---
+++ covdata/R/key_crop_yields.R
@@ -0,0 +1,9 @@
+#'Analyze Cases With Countries In Cov19
+
+#'This data include 379 countries in the world
+
+#'@source
+#'https://github.com/RamiKrispin/coronavirus
+#'The coronavirus package provides a tidy format dataset of the 2019 Novel Coronavirus COVID-19 (2019-nCoV) epidemic. The raw data pulled from the Johns Hopkins University Center for Systems Science and Engineering (JHU CCSE) Coronavirus repository.
+#'
+"key_crop_yields"
